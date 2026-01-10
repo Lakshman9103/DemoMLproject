e647e54f7236ab07bb03946c4e68027ca4a285db
Lakshman Prabhu Palanisamy <lakshman9103@gmail.com>
2026-01-10 11:08:24 +0530

    Added exception, logger, utils and components and pipeline folder under SRC

---
+++ logger.py
@@ -0,0 +1,16 @@
+import logging
+import os
+from datetime import datetime
+
+Log_Filename = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+logs_path = os.path.join("logs", Log_Filename)
+os.makedirs(logs_path, exist_ok=True)   
+
+log_file_path = os.path.join(logs_path, Log_Filename)
+
+logging.basicConfig(
+    filename=log_file_path, 
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s - %(message)s', 
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
